daf7403bd2f33b3d9039732e207d759c241afe0a
NickShablin <sl1pp3r15@yandex.ru>
2025-06-27 11:27:53 +0300

    [Prolog individual task] - topological sort
    Update: recursion for edges

--- Prolog/topological_sort/topo_sort.pl
+++ Prolog/topological_sort/topo_sort.pl
@@ -5,3 +5,9 @@ can_topo_sort(Edges) :-
     ->  writeln('can not make a topological sort - a cycle found')
     ;   writeln('can make a topological sort')
     ).
+
+assert_edges([]).
+assert_edges([X-Y | Es]) :-
+    assert(edge(X,Y)),
+    assert_edges(Es).
+
